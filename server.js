8fa11c733590a443672beb13aaacdba5988478f4
Faris Shamsudeen <faris52342345@gmail.com>
2025-02-25 11:17:59 +0530

    modified server.js

--- server.js
+++ server.js
@@ -1,48 +1,49 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const cors = require("cors");
 
 const app = express();
-const server = http.createServer(app);
-// const io = socketIo(server);
-const io = socketIo(server, {
-    cors: {
-        origin: "*",  // Allow all origins or specify your Vercel app domain
-        methods: ["GET", "POST"]
-    }
+
+
+const server = app.listen(0, () => {
+  console.log(
+    `🚀 Server is live at: http://localhost:${server.address().port}`
+  );
 });
 
-const PORT = process.env.PORT || 6000
-const path = require('path')
-const cors = require("cors");
+
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // Allow all origins or specify your Vercel app domain
+    methods: ["GET", "POST"],
+  },
+});
+
+const path = require("path");
+
 app.use(cors());
-
-
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 io.on("connection", (socket) => {
-    console.log("New user connected");
+  console.log("New user connected");
 
-    socket.on("joinChat", (username) => {
-        socket.username = username;
-        io.emit("userJoined", username);
-    });
+  socket.on("joinChat", (username) => {
+    socket.username = username;
+    io.emit("userJoined", `${username}`);
+  });
 
-    socket.on("chatMessage", ({ username, message }) => {
-        io.emit("chatMessage", { username, message });
-    });
+  socket.on("chatMessage", ({ username, message }) => {
+    io.emit("chatMessage", { username, message });
+  });
 
-    socket.on("disconnect", () => {
-        if (socket.username) {
-            io.emit("userJoined", `${socket.username} left the chat`);
-        }
-    });
-});
-
-server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      io.emit("userJoined", `${socket.username} left the chat`);
+    }
+  });
 });
