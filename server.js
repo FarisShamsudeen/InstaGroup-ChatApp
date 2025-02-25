fe6c622481583ef098dd230f5f1b48e42096551c
Faris Shamsudeen <faris52342345@gmail.com>
2025-02-25 10:13:17 +0530

    added root route in server.js"

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const socketIo = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on("connection", (socket) => {
     console.log("New user connected");
