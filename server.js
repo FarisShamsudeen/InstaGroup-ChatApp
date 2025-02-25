3c86558ae228ce3b93bc0bd1419ae2c8a132b53f
Faris Shamsudeen <faris52342345@gmail.com>
2025-02-25 10:15:26 +0530

    changed server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const socketIo = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6000
 
 app.use(express.static("public"));
 
